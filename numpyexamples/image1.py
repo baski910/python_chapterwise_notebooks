1b513e819bdcc5cfdf8842075c822a9357595e51
baski910 <42275121+baski910@users.noreply.github.com>
2023-03-16 01:59:44 +0530

    Update image1.py

--- numpyexamples/image1.py
+++ numpyexamples/image1.py
@@ -16,6 +16,7 @@ plt.show()
 print(pic[0,959,:])
 
 # [72,86,53] * [0.299,0.587,0.114] = [72*0.299 + 86*0.587 + 53*0.114]
+# [0.299,0.587,0.114] - factor used by GNU Image Manipulation Program 
 gray = lambda img: np.dot(img[...],[0.299,0.587,0.114])
 
 wb = gray(pic)
