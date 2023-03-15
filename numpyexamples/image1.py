09d1c1fdc8813d0fc7496daa98daf6f77912f74c
baski910 <42275121+baski910@users.noreply.github.com>
2023-03-16 01:52:06 +0530

    Create image1.py

---
+++ numpyexamples/image1.py
@@ -0,0 +1,25 @@
+# storing images as numpy array
+import imageio.v2 as imageio # pip install imageio
+import numpy as np
+import matplotlib.pyplot as plt
+
+pic = imageio.imread("demo_2.jpg")
+
+print(type(pic))
+
+print(pic.shape)
+
+plt.figure(figsize=(12,10))
+plt.imshow(pic)
+plt.show()
+
+print(pic[0,959,:])
+
+# [72,86,53] * [0.299,0.587,0.114] = [72*0.299 + 86*0.587 + 53*0.114]
+gray = lambda img: np.dot(img[...],[0.299,0.587,0.114])
+
+wb = gray(pic)
+
+plt.figure(figsize=(12,10))
+plt.imshow(wb, cmap=plt.get_cmap(name='gray'))
+plt.show()
