175c1f092d0fea13cf9fa1bf8aac50c0865cd7af
baski910 <42275121+baski910@users.noreply.github.com>
2023-08-11 10:09:25 +0530

    Create selectdb.py

---
+++ dbexamples/selectdb.py
@@ -0,0 +1,22 @@
+# connect to postgresql database
+# pip install psycopg2
+import sys
+import psycopg2 # cx_Oracle
+
+conn = psycopg2.connect(host='localhost',user='demodbuser_1',password='password',dbname='demodb_1')
+
+if not conn:
+    sys.exit()
+
+cur = conn.cursor()
+
+try:
+    cur.execute("select * from books")
+except Exception as e:
+    print(e)
+else:
+    records = cur.fetchall() # cur.fetchone()
+    for row in records:
+        print(f"ID:row[0],Title:row[1]")
+finally:
+    conn.close()
