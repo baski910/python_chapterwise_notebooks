bc80ba59b5477bb0e3b0c463add21549df7b6aae
baski910 <42275121+baski910@users.noreply.github.com>
2024-01-12 14:34:27 +0530

    Create example2.py
    added opening an existing excel file

---
+++ openpyexamples/example2.py
@@ -0,0 +1,19 @@
+import openpyxl
+
+book = openpyxl.load_workbook('sample.xlsx')
+
+sheet = book.active
+
+a1 = sheet['A1']
+a2 = sheet['A2']
+a3 = sheet.cell(row=3, column=1)
+
+print(a1.value)
+print(a2.value) 
+print(a3.value)
+
+
+sheet1 = book.create_sheet('Sheet2')
+
+
+book.save('sample.xlsx')
