fbc971c9429a183d0156afbdc62edc1fcb77de3a
baski910 <42275121+baski910@users.noreply.github.com>
2024-06-21 16:08:33 +0530

    Create __init__.py

---
+++ flaskrestexamples/project2/app/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, request, render_template
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+db = SQLAlchemy()
+ma = Marshmallow()
+
+def create_app():
+    app = Flask(__name__,instance_path=os.getcwd(),instance_relative_config=True)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+    ma.init_app(app)
+    CORS(app)
+    Migrate(app,db)
+
+    return app
