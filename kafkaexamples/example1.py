fafc79cce9fd81429430f8dd3c1c053c87344d22
baski910 <42275121+baski910@users.noreply.github.com>
2025-05-01 19:05:52 +0530

    Update example1.py

--- kafkaexamples/example1.py
+++ kafkaexamples/example1.py
@@ -1,1 +1,16 @@
 # to be added
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+producer.send('my_topic',key=b'my_key',value=b'Hello Kafka')
+
+producer.flush()
+
+
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer('my_topic',bootstrap_servers='localhost:9092')
+for message in consumer:
+    print(message)
+
