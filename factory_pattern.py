5ea8ba40e60cd435a66c1c5f35e4c355c2469872
baski910 <42275121+baski910@users.noreply.github.com>
2025-05-07 17:08:28 +0530

    Create factory_pattern.py

---
+++ factory_pattern.py
@@ -0,0 +1,35 @@
+class Song:
+    def __init__(self, song_id, title, artist):
+        self.song_id = song_id
+        self.title = title
+        self.artist = artist
+
+
+class SongSerializer:
+    def serialize(self, song, format):
+        if format == 'JSON':
+            song_info = {
+                'id': song.song_id,
+                'title': song.title,
+                'artist': song.artist
+            }
+            return json.dumps(song_info)
+        elif format == 'XML':
+            song_info = et.Element('song', attrib={'id': song.song_id})
+            title = et.SubElement(song_info, 'title')
+            title.text = song.title
+            artist = et.SubElement(song_info, 'artist')
+            artist.text = song.artist
+            return et.tostring(song_info, encoding='unicode')
+        else:
+            raise ValueError(format)
+            
+
+import serializer_demo as sd
+song = sd.Song('1', 'Water of Love', 'Dire Straits')
+serializer = sd.SongSerializer()
+
+serializer.serialize(song, 'JSON')
+'{"id": "1", "title": "Water of Love", "artist": "Dire Straits"}'
+
+serializer.serialize(song, 'XML')
