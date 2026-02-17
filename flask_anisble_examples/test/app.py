81a98e1c262bc6e76f54e4499435151ebb850279
baski910 <42275121+baski910@users.noreply.github.com>
2026-02-17 16:48:50 +0530

    Update app.py

--- flask_anisble_examples/test/app.py
+++ flask_anisble_examples/test/app.py
@@ -1,16 +1,20 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
 @app.route('/') # http://127.0.0.1:5000
 def index():
-    return "<h1>helloworld</h1>"
+    return render_template("my.html")
+    #return "<h1>helloworld</h1>"
 
 @app.route('/showMessage',methods=['POST'])
 def message():
-    data = request.get_json()
-    name = data.get('name','')
-    return f"<h1>Welcoem flask user {name}</h1>"
+    #data = request.get_json()
+    #name = data.get('name','')
+    playbook =  request.form['playbook']
+    password = request.form['password']
+    return f"<h1>Playbook - {playbook} and Password = {password}</h1>"
+    #return f"<h1>Welcoem flask user {name}</h1>"
 
 if __name__ == "__main__":
     app.run()
