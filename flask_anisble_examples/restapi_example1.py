c9d1ce8d5dac95ab65fcc8bd9967034600f46967
baski910 <42275121+baski910@users.noreply.github.com>
2026-02-17 14:15:27 +0530

    Update restapi_example1.py

--- flask_anisble_examples/restapi_example1.py
+++ flask_anisble_examples/restapi_example1.py
@@ -24,7 +24,7 @@ def run_playbook_task(job_id, playbook_path, extra_vars):
             extravars={
                 'ansible_become': True,
                 'ansible_become_method': 'sudo',
-                'ansible_become_user': 'student',
+                'ansible_become_user': 'root',
                 'ansible_become_pass': 'P@%%w0rd@26',
             },
             json_mode=True
