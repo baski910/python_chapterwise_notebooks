caba4b33ac0089d51b89936a44c3e50ed27e2591
baski910 <42275121+baski910@users.noreply.github.com>
2026-02-19 12:21:53 +0530

    Update tasks.py

--- flask_anisble_examples/celeryexamples/tasks.py
+++ flask_anisble_examples/celeryexamples/tasks.py
@@ -8,5 +8,15 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0',backend='redis://
 def run_ansible_playbook(playbook_path, inventory_path):
     # Run ansible-runner
     inv_path = os.path.join(os.getcwd(),f"project2/inventory/{inventory_path}")
-    r = ansible_runner.run(private_data_dir='./project2', playbook=playbook_path, inventory=inv_path)
+    r = ansible_runner.run(
+        private_data_dir='./project2', 
+        playbook=playbook_path, 
+        inventory=inv_path,
+        extravars={
+            'ansible_become': True,
+            'ansible_become_method': 'sudo',
+            'ansible_become_user': 'root',
+            'ansible_become_pass': 'P@%%w0rd@26'
+        }
+    )
     return {"status": r.status,"stats":r.stats, "rc": r.rc}
